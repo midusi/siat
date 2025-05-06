804a1183f0bf17ad75b4df0c1215f184deafd664
Nacho Zampelunghe <nzampelunghe@gmail.com>
2025-05-06 15:36:48 -0300

    type List -> list

--- backend/app/crud/district.py
+++ backend/app/crud/district.py
@@ -1,7 +1,6 @@
 # crud/district.py
 from sqlalchemy.orm import Session
 from app.models import District
-from typing import List
 
-def get_list(db: Session) -> List[District]:
+def get_list(db: Session) -> list[District]:
     return db.query(District).all()
--- backend/app/crud/locality.py
+++ backend/app/crud/locality.py
@@ -1,10 +1,9 @@
 # crud/locality.py
 from sqlalchemy.orm import Session
 from app.models import Locality
-from typing import List
 
 def get_localities():
     return [{"id": 1, "name": "Juan"}]
 
-def get_localities_by_district(db: Session, district_id: int):
+def get_localities_by_district(db: Session, district_id: int) -> list[Locality]:
     return db.query(Locality).filter(Locality.district_id == district_id).all()
