1dfdcfa1e7e9a1ceb3f7705fe67a7cee0ff4d06c
Nacho Zampelunghe <nzampelunghe@gmail.com>
2025-06-06 11:51:49 -0300

    Add security with cookies

--- backend/app/auth/middleware.py
+++ backend/app/auth/middleware.py
@@ -5,10 +5,15 @@ from app.auth.jwt import decode_token
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        token = None
         auth = request.headers.get("Authorization")
         if auth and auth.startswith("Bearer "):
             token = auth[7:]
+        elif request.cookies.get("access_token"):
+            token = request.cookies.get("access_token")
+        if token:
             user_data = decode_token(token)
+            print(user_data)
             if user_data:
                 request.state.user = user_data
         return await call_next(request)
